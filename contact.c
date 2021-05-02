bea7f0f5fcb1a662e42b1fcba2ed3a5e28473d18
63070158 <71114538+63070158@users.noreply.github.com>
2021-05-02 13:43:27 +0700

    Update contact.c

--- contact.c
+++ contact.c
@@ -16,6 +16,12 @@ char query[20],name[20];
 FILE *fp, *ft;
 int i,n,ch,l,found;
 
-int main()
-
+int main(){
+    main:
+    // Creating Main Menu Interface krub
+    system("cls");    
+    printf("\n\t <<< Contact Management System by GFKK >>>");
+    printf("\n\n\t\t\tM-E-N-U\n\t\t<+-+-+-+-+-+-+-+-+-+>\n\t\t[1] Add\n\t\t[2] List all\n\t\t[3] Search\n\t\t[4] Edit\n\t\t[5] Delete\n\t\t[0] Exit\n\t\t<+-+-+-+-+-+-+-+-+-+>\n\t\t");
+    printf("Enter number: ");
+    scanf("%d",&ch);
 {
