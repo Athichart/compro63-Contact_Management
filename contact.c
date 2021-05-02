6b2a815e7b26362a79a95757fc5cdb196e9bd6bf
Athichart <73677899+Athichart@users.noreply.github.com>
2021-05-02 15:08:37 +0700

    Update contact.c

--- contact.c
+++ contact.c
@@ -64,7 +64,75 @@ main:
         fclose(fp);
         break;
 
+        /* *********************edit contacts************************/
 
+    case 4:
+
+        system("cls");
+
+        fp=fopen("contact.dll","r");
+
+        ft=fopen("temp.dat","w");
+
+        fflush(stdin);
+
+        printf("..::Edit contact\n");
+        printf("===============================\n\n");
+        printf("\t..::Enter the name of contact to edit:");
+
+        scanf("%[^\n]",name);
+
+        while(fread(&list,sizeof(list),1,fp)==1)
+
+        /*check name in list*/
+        {
+
+            if(stricmp(name,list.name)!=0)
+
+                fwrite(&list,sizeof(list),1,ft);
+
+        }
+
+        fflush(stdin);
+
+        printf("\n\n..::Editing '%s'\n\n",name);
+
+        printf("..::Name(Use identical):");
+
+        scanf("%[^\n]",&list.name);
+
+        fflush(stdin);
+
+        printf("..::Phone:");
+
+        scanf("%ld",&list.phone);
+
+        fflush(stdin);
+
+        printf("..::address:");
+
+        scanf("%[^\n]",&list.add);
+
+        fflush(stdin);
+
+        printf("..::email address:");
+
+        gets(list.email);
+
+        printf("\n");
+
+        fwrite(&list,sizeof(list),1,ft);
+
+        fclose(fp);
+
+        fclose(ft);
+
+        remove("contact.dll");
+
+        rename("temp.dat","contact.dll");
+
+        break;
+            
     default:
         printf("Invalid Choice");
         break;
